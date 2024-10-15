c4d96968952ebd61bf076ebb525a73722acae356
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-10-15 23:19:05 +0530

    nit

--- data_collection.sh
+++ data_collection.sh
@@ -2,3 +2,4 @@ sudo apt install softflowd
 sudo softflowd -i wlo1 -n 127.0.0.1:9995
 mkdir netflow
 sudo nfcapd -D -w ./netflow -p 9995
+echo "Data collection started, please check netflow folder for data after five minutes"
